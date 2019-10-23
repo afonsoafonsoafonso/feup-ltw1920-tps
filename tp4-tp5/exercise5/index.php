808c1795d298d0a347438c98febd06af2f15622d
afonsoafonsoafonso <afonsomendonca91@gmail.com>
2019-10-23 14:37:31 +0100

    awdawd

--- tp4-tp5/exercise5/index.php
+++ tp4-tp5/exercise5/index.php
@@ -48,35 +48,8 @@
       </article>
     </aside>
     <section id="news">
-        <?php
-        $db = new PDO('sqlite:news.db');
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-      
-        $stmt = $db->prepare('SELECT * FROM news');
-        $stmt->execute();
-        $articles = $stmt->fetchAll();
-      
-        foreach($articles as $article) {
-            echo '<article>';
-            echo '<header>';
-            echo '<h1>' . $article['title'] .' </h1>';
-            echo '</header>';
-            echo '<img src="http://lorempixel.com/600/300/business/" alt="">';
-            echo '<p>' . $article['introduction'] . '</p>';
-            echo '<p>' . $article['fulltext'] . '</p>';
-            echo '<footer>';
-            echo '<span class="author">' . $article['username'] . '</span>';
-            echo '<span class="tags">'; 
-            $tags = explode(",", $article['tags']);
-            foreach($tags as $tag) {
-                echo '<a href="index.php"> #' . $tag . ' ' . '</a>';
-            } 
-            echo '</span>';
-            echo '<span class="date">' . substr(gmdate('r', $article['published']), 0, -5) . '</span>'; 
-            echo '</footer>';
-            echo '</article>';
-        }
+      <?php
+        include('list_news.php');
       ?>
     </section>
     <footer>
--- tp4-tp5/exercise5/list_news.php
+++ tp4-tp5/exercise5/list_news.php
@@ -1,12 +1,28 @@
 <?php
 	$db = new PDO('sqlite:news.db');
 
-	$stmt = $db->prepare('SELECT * FROM news');
+	$stmt = $db->prepare('SELECT * FROM news	');
 	$stmt->execute();
 	$articles = $stmt->fetchAll();
 
 	foreach($articles as $article) {
+		echo '<article>';
+		echo '<header>';
 		echo '<h1>' . $article['title'] .' </h1>';
+		echo '</header>';
+		echo '<img src="http://lorempixel.com/600/300/business/" alt="">';
 		echo '<p>' . $article['introduction'] . '</p>';
+		echo '<p>' . $article['fulltext'] . '</p>';
+		echo '<footer>';
+		echo '<span class="author">' . $article['username'] . '</span>';
+		echo '<span class="tags">'; 
+		$tags = explode(",", $article['tags']);
+		foreach($tags as $tag) {
+			echo '<a href="index.php"> #' . $tag . ' ' . '</a>';
+		} 
+		echo '</span>';
+		echo '<span class="date">' . substr(gmdate('r', $article['published']), 0, -5) . '</span>'; 
+		echo '</footer>';
+		echo '</article>';
 	}
 ?>
